5cea05f8342ec95b02b7074865c4c785d1315710
bermog <code@bermog.com>
2024-05-06 03:09:23 +0200

    Introduce route model binding

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,7 @@ Route::get('/jobs/create', function () {
 });
 
 // Show
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-
+Route::get('/jobs/{job}', function (Job $job) {
     return view('jobs.show', ['job' => $job]);
 });
 
@@ -45,22 +43,18 @@ Route::post('/jobs', function () {
 });
 
 // Edit
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::find($id);
-
+Route::get('/jobs/{job}/edit', function (Job $job) {
     return view('jobs.edit', ['job' => $job]);
 });
 
 // Update
-Route::patch('/jobs/{id}', function ($id) {
+Route::patch('/jobs/{job}', function (Job $job) {
+    // authorize pending
+
     request()->validate([
         'title' => ['required', 'min:3'],
         'salary' => ['required'],
     ]);
-
-    // authorize pending
-
-    $job = Job::findOrFail($id);
 
     $job->update([
         'title' => request('title'),
@@ -71,10 +65,10 @@ Route::patch('/jobs/{id}', function ($id) {
 });
 
 // Destroy
-Route::delete('/jobs/{id}', function ($id) {
+Route::delete('/jobs/{job}', function (Job $job) {
     // authorize pending
 
-    Job::findOrFail($id)->delete();
+    $job->delete();
 
     return redirect('/jobs');
 });

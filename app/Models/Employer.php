c67f9210b8a9582c7a25e3e936d13692d9d7b859
bermog <code@bermog.com>
2024-05-09 23:28:11 +0200

    Update model classes with IDE helper

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperEmployer
+ */
 class Employer extends Model
 {
     use HasFactory;
